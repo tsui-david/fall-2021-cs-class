ba4f9548973e3c06c835b11076ac1319e51f6a3d
David Tsui <david.r.tsui@gmail.com>
2021-09-26 10:08:00 -0400

    first class

---
+++ class1/main.py
@@ -0,0 +1,38 @@
+"""
+Exercise 0. This is an example
+"""
+
+## vvvvvvvvvv  Do your code here vvvvvvvvv
+
+print('Hello!')
+
+## ^^^^  Comment your code before you move on ^^^^^^
+
+
+"""
+Exercise 1. Create a custom calculator for calculating area of a square and print it
+
+Ex:
+
+Enter length of side of the square: 2
+The area is 4
+"""
+
+## vvvvvvvvvv  Do your code here vvvvvvvvv
+
+
+## ^^^^  Comment your code before you move on ^^^^^^
+
+""""
+Exercise 2. Create a custom calculator for calculating area of trapezoid
+
+Enter the height of the trapezoid: 5
+Enter the length of the bottom base: 10
+Enter the length of the top base: 7
+The area is: 42.5
+"""
+
+## vvvvvvvvvv  Do your code here vvvvvvvvv
+
+
+## ^^^^  Comment your code before you move on ^^^^^^
