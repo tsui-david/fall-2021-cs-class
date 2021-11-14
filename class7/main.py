c486eeedbffb32cd407860ee3055977aa984f6e2
David Tsui <david.r.tsui@gmail.com>
2021-11-14 13:00:22 -0500

    class7

---
+++ class7/main.py
@@ -0,0 +1,41 @@
+"""
+Exercise 1:
+
+Create a custom array program which allows user to add an integer.
+Then print the array after the user made their choice
+
+Example:
+
+Enter an integer: 4
+[4]
+Enter an integer: 5
+[4, 5]
+Enter an integer: 3
+[4, 5, 3]
+Enter an integer: 1
+[4, 5, 3, 1]
+Enter an integer: 8
+[4, 5, 3, 1, 8]
+
+
+Exercise 2. 
+Add an extra feature: allow user to modify the list given an index.
+
+Example:
+
+Do you want to add or edit? add
+Enter an integer: 3
+[4, 5, 3, 1, 8, 3]
+Do you want to add or edit? edit
+Which index do you want to modify? 0
+Enter updated integer: -1
+[-1, 5, 3, 1, 8, 3]
+
+
+Exercise 3.
+Add an extra feature: allow user option to sum all the values.
+
+Example:
+Do you want to add or edit or sum? sum
+[-1, 5, 3, 1, 8, 3] = 19
+"""
